4167ba96697d4206b61d4c4a858757b88e2f911a
HMZ <hamzi1995@gmail.com>
2021-04-12 13:50:45 -0400

    Initial commit

---
+++ wwwroot/js/modules/animejsModule.js
@@ -0,0 +1,47 @@
+﻿export function InitAnimeJs() {
+    //ProcessText();
+    //StartAnimation();
+}
+
+function ProcessText() {
+    let children = Array.from(document.getElementsByClassName("hero-text")[0].children);
+    let content = [];
+
+    children.forEach(function (ele, index) {
+        content[index] = function () {
+            let html = "";
+            ele.textContent.split("").forEach(function (letter, index) {
+                if (letter == " ") {
+                    return true;
+                }
+
+                html += `<span class="letter">${letter}</span>`;
+            });
+
+            return html;
+        }();
+    });
+
+    children[0].innerHTML = content[0];
+    children[1].innerHTML = content[1];
+    children[2].innerHTML = content[2];
+}
+
+function StartAnimation() {
+    let animeTargets = document.querySelectorAll(".hero-text .letter");
+
+    let animeTL = anime.timeline({
+        duration: 5000
+    });
+
+    animeTL.add({
+        targets: animeTargets,
+        translateX: [-1000, 0],
+        translateY: [-500, 0],
+        //scale: function (el, i, l) { return 1.5; },
+        //borderRadius: function () { return ['50%', anime.random(10, 35) + '%']; },
+        duration: function () { return anime.random(1200, 1800); },
+        delay: function (el, i, l) { return anime.random(0, 400); },
+        direction: 'forward'
+    });
+}
---
+++ wwwroot/js/modules/navModule.js
@@ -0,0 +1,67 @@
+﻿var body = document.querySelector('body');
+var navbar = document.getElementById('navbar');
+var navBtn = document.getElementById("navBtn");
+var sideNavRef;
+var lastScrollPos;
+
+export function PassSideNavRef(SideNavRef) {
+    sideNavRef = SideNavRef;
+}
+
+export function InitNavbarEventListeners() {
+
+    if (!IsNavRefPassed()) {
+        return false;
+    }
+
+    body.addEventListener("click", function (e) {
+        let ele = e.target;
+
+        if (ele.closest("#navbar") == null && ele.closest("#sidenav") == null) {
+            return sideNavRef.invokeMethodAsync("CloseMenu");
+        }
+    });
+
+    InitScrollListener();
+    InitMouseTracker();
+    RetractNavbar();
+}
+
+function IsNavRefPassed() {
+    return sideNavRef != null;
+}
+
+function InitScrollListener() {
+    document.addEventListener("scroll", function (e) {
+        if (window.scrollY > lastScrollPos) {
+            RetractNavbar();
+        }
+
+        lastScrollPos = window.scrollY;
+    });
+}
+
+function InitMouseTracker() {
+    document.addEventListener("mousemove", function (e) {
+        if (e.clientX <= 70) {
+            AdvanceNavbar();
+        }
+        else {
+            RetractNavbar();
+        }
+    });
+}
+
+function AdvanceNavbar() {
+    if (!(sideNavRef.invokeMethod("IsMenuOpen"))) {
+        navbar.classList.remove("is-open");
+        navBtn.removeAttribute("disabled");
+    }
+}
+
+function RetractNavbar() {
+    if (!(sideNavRef.invokeMethod("IsMenuOpen"))) {
+        navbar.classList.add("is-open");
+        navBtn.setAttribute("disabled", true);
+    }
+}
