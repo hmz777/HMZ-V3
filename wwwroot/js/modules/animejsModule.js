45a87e2cbf19ef6bb84287778d040eb63be32995
HMZ <hamzi1995@gmail.com>
2021-04-13 11:38:38 -0400

    2nd batch of changes

--- wwwroot/js/modules/animejsModule.js
+++ wwwroot/js/modules/animejsModule.js
@@ -1,6 +1,6 @@
 ﻿export function InitAnimeJs() {
     //ProcessText();
-    //StartAnimation();
+    StartAnimation();
 }
 
 function ProcessText() {
@@ -28,20 +28,45 @@ function ProcessText() {
 }
 
 function StartAnimation() {
-    let animeTargets = document.querySelectorAll(".hero-text .letter");
+    let animeCardTargets = document.querySelectorAll(".hero-intro .hero-card");
+    let animeTextTargets = document.querySelectorAll(".hero-intro .hero-card .text");
+    let animeListTargets = document.querySelectorAll(".hero-list .hero-card");
 
     let animeTL = anime.timeline({
-        duration: 5000
+        duration: 2000
     });
 
     animeTL.add({
-        targets: animeTargets,
+        targets: animeListTargets,
         translateX: [-1000, 0],
         translateY: [-500, 0],
-        //scale: function (el, i, l) { return 1.5; },
-        //borderRadius: function () { return ['50%', anime.random(10, 35) + '%']; },
+        opacity: [0, 1],
+        scale: function (el, i, l) { return 0.6; },
         duration: function () { return anime.random(1200, 1800); },
         delay: function (el, i, l) { return anime.random(0, 400); },
         direction: 'forward'
+    }).add({
+        targets: animeCardTargets,
+        translateX: [-1000, 0],
+        translateY: [-500, 0],
+        opacity: [0, 1],
+        borderRadius: function () { return 50 + "%"; },
+        scale: function (el, i, l) { return 0.6; },
+        duration: function () { return anime.random(1200, 1800); },
+        delay: function (el, i, l) { return anime.random(0, 400); },
+        direction: 'forward'
+    }).add({
+        targets: animeCardTargets,
+        borderRadius: function () { return 1 + "em"; },
+        scale: function (el, i, l) { return 1; },
+        width: function (el, i, l) { if (i == 0) return 36 + "em"; if (i == 1) return 45 + "em"; if (i == 2) return 52 + "em"; },//38-51
+        height: function () { return 15 + "em"; },
+    }).add({
+        targets: animeTextTargets,
+        opacity: function () { return 1; }
     });
+}
+
+function TechTrigger() {
+
 }
